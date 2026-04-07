d72dacab12562b6a2f9522b527b156d72fcba08f
vzepify <142155258+vzepify@users.noreply.github.com>
2026-04-07 10:13:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const app = express();
 app.use(express.json());
